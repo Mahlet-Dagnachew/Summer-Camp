4e5a19e7aa7e2cabd29c8a37fd1bbfaa96258cee
kaleab-taye <kaleabtaye@gmail.com>
2022-08-29 09:24:49 +0300

    files

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "80%";
+	    document.getElementById("nav-close").style.width = "20%";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	    document.getElementById("nav-close").style.width = "0";
+
+}
